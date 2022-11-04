d96dbeee50b41377cdf20d0901b1e8fd03953c00
Seth Miller <millerado@gmail.com>
2022-11-04 10:56:00 -0600

    completed problem

---
+++ 412-fizz-buzz.js
@@ -0,0 +1,33 @@
+/*
+https://leetcode.com/problems/fizz-buzz/
+
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+
+ @param {number} n
+ @return {string[]}
+*/
+
+var fizzBuzz = function (n) {
+  let output = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      output.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      output.push('Fizz');
+    } else if (i % 5 === 0) {
+      output.push('Buzz');
+    } else {
+      output.push(`${i}`);
+    }
+  }
+  return output;
+};
+
+console.log(fizzBuzz(15));
+
+// Accepted! Runtime 64 ms.
