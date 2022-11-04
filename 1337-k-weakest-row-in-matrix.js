acc1edd4d2082a9f64f098ba679e06d50d7a6472
Seth Miller <millerado@gmail.com>
2022-11-04 11:15:03 -0600

    problem in progress

---
+++ 1337-k-weakest-row-in-matrix.js
@@ -0,0 +1,47 @@
+/*
+https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+
+You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). The soldiers are positioned in front of the civilians. That is, all the 1's will appear to the left of all the 0's in each row.
+
+A row i is weaker than a row j if one of the following is true:
+
+The number of soldiers in row i is less than the number of soldiers in row j.
+Both rows have the same number of soldiers and i < j.
+Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+
+Constraints:
+m == mat.length
+n == mat[i].length
+2 <= n, m <= 100
+1 <= k <= m
+matrix[i][j] is either 0 or 1.
+
+@param {number[][]} mat
+@param {number} k
+@return {number[]}
+*/
+
+var kWeakestRows = function (mat, k) {
+  let strength = [];
+  let output = [];
+  for (let i = 0; i < mat.length; i++) {
+    let sum = 0;
+    output.push(i);
+    for (let j = 0; j < mat[i].length; j++) {
+      sum += mat[i][j];
+    }
+    strength.push(sum);
+  }
+  console.log(strength, output);
+};
+
+kWeakestRows(
+  [
+    [1, 1, 0, 0, 0],
+    [1, 1, 1, 1, 0],
+    [1, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0],
+    [1, 1, 1, 1, 1],
+  ],
+  3
+);
