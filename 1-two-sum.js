779e7bd07ae3ad5bbe58738b5cd04e3198a4be79
Seth Miller <millerado@gmail.com>
2023-01-24 09:26:02 -0700

    finished problem 1

---
+++ 1-two-sum.js
@@ -0,0 +1,24 @@
+/* 
+Given an array of integers (nums) and an integer (target), return indices of the two number such 
+as they add up to (target).
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const numsIdx = [];
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      if (i < j && nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+twoSum([3, 2, 4], 6);
+
+// Accepted! Runtime 150 ms
