97a7ab2be6af7440fe779b89f4ffd7fad25c3967
Seth Miller <millerado@gmail.com>
2023-02-21 11:14:14 -0700

    completed problem

---
+++ 540-single-element-in-a-sorted-array.js
@@ -0,0 +1,25 @@
+/*
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+Return the single element that appears only once.
+
+Your solution must run in O(log n) time and O(1) space.
+*/
+
+var singleNonDuplicate = function (nums) {
+  let stack = [];
+  if (nums.length === 1) return nums[0];
+  for (let i = 0; i < nums.length; i++) {
+    stack.push(nums[i]);
+    if (stack.length === 2 && stack[0] !== stack[1]) {
+      return stack[0];
+    } else if (stack.length === 2) {
+      stack = [];
+    }
+  }
+  if (stack.length === 1) return stack[0];
+};
+
+singleNonDuplicate([1, 1, 2]);
+
+// Accepted! Runtime 123ms (Very Slow!)
