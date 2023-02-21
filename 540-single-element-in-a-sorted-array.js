c36c7046e9847cce6e8f1eb08f5c7d333f773e0d
Seth Miller <millerado@gmail.com>
2023-02-21 11:29:09 -0700

    improved solution

--- 540-single-element-in-a-sorted-array.js
+++ 540-single-element-in-a-sorted-array.js
@@ -6,6 +6,7 @@ Return the single element that appears only once.
 Your solution must run in O(log n) time and O(1) space.
 */
 
+/*
 var singleNonDuplicate = function (nums) {
   let stack = [];
   if (nums.length === 1) return nums[0];
@@ -19,7 +20,19 @@ var singleNonDuplicate = function (nums) {
   }
   if (stack.length === 1) return stack[0];
 };
-
-singleNonDuplicate([1, 1, 2]);
+*/
 
 // Accepted! Runtime 123ms (Very Slow!)
+
+// Better Version
+var singleNonDuplicate = function (nums) {
+  for (let i = 0; i < nums.length - 1; i += 2) {
+    if (nums[i] !== nums[i + 1]) {
+      return nums[i];
+    }
+  }
+  return nums[nums.length - 1];
+};
+singleNonDuplicate([1, 1, 2]);
+
+// Accepted! Runtime 76 ms.
